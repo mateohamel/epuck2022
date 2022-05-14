ab93a4bfaa6bb30ad262847fef3515f66bcddfec
romaneclrc <104548713+romaneclrc@users.noreply.github.com>
2022-05-14 20:53:19 +0200

    Merge branch 'main' of https://github.com/mateohamel/epuck2022

---
+++ selection.h
@@ -0,0 +1,15 @@
+/*
+ * selection.h
+ *
+ *  Created on: 14 mai 2022
+ *      Author: hamel
+ */
+
+#ifndef SELECTION_H_
+#define SELECTION_H_
+
+void mode_select_init(void);
+
+
+
+#endif /* SELECTION_H_ */
