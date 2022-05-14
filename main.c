bbe15de7e233cd7804ec0503121b05ee7269d464
romaneclrc <104548713+romaneclrc@users.noreply.github.com>
2022-05-14 20:59:34 +0200

    Revert "as the branch name says"
    This reverts commit f89b4db17dd056688c7bfd93d14b5d54d3bd5cfa.

--- main.c
+++ main.c
@@ -53,66 +53,6 @@ CONDVAR_DECL(bus_condvar);
 static direction route[35];
 static uint8_t size = 0;
 
-instruction get_instruction_flow(uint8_t index){
-	return g_instruction_flow[index];
-}
-
-void set_instruction_flow(instruction new_instruction, uint8_t index){
-	g_instruction_flow[index] = new_instruction;
-	return;
-}
-
-
-uint8_t get_instruction_counter(void){
-	return g_instruction_counter;
-}
-
-void set_instruction_counter(uint8_t new_instruction_counter){
-	g_instruction_counter = new_instruction_counter;
-	return;
-}
-
-void increase_instruction_counter(void){
-	g_instruction_counter++;
-	return;
-}
-
-
-direction get_route(uint8_t index){
-	return g_route[index];
-}
-
-void set_route(direction new_direction, uint8_t index){
-	g_route[index] = new_direction;
-	return;
-}
-
-
-uint8_t get_route_counter(void){
-	return g_route_counter;
-}
-
-void set_route_counter(uint8_t new_route_counter){
-	g_route_counter = new_route_counter;
-	return;
-}
-
-void increase_route_counter(void){
-	g_route_counter++;
-	return;
-}
-
-
-mode get_mode(void){
-	return g_mode;
-}
-
-void set_mode(mode new_mode){
-	g_mode = new_mode;
-	return;
-}
-
-
 void go(direction dir, uint8_t motor_speed){
 
 	switch(dir){
